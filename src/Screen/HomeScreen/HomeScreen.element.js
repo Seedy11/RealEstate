7ce9bcc2eb24b383b49bcee0a135c040fc6ee6f0
Seedy11 <64992816+Seedy11@users.noreply.github.com>
2024-04-14 16:28:48 +0100

    xd

--- src/Screen/HomeScreen/HomeScreen.element.js
+++ src/Screen/HomeScreen/HomeScreen.element.js
@@ -1,19 +1,18 @@
+/** @format */
+
 import styled from "styled-components";
 
-const ImageWrapper = styled.h2 `
- color: white;
+const ImageWrapper = styled.h2`
+  color: white;
 
- img {
+  img {
     height: 500px;
     width: 100%;
     object-fit: cover;
-   
+
     border-radius: 20px;
     align-item: center;
-    
- }
- 
-  
-`
+  }
+`;
 
-export default ImageWrapper
+export default ImageWrapper;
